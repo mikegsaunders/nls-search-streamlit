2e8150f490b147697e9cdbbd08605b41e211b159
mikegsaunders <m.saunders@nls.uk>
2024-12-17 10:41:42 +0000

    added mutation observer

--- search.py
+++ search.py
@@ -24,10 +24,33 @@ st.markdown(hide_st_style, unsafe_allow_html=True)
 # js
 js = """
 <script>
-var profile = document.querySelector("._profilePreview_gzau3_63");
-profile.style.display = "none";
-var comm = document.querySelector("._link_gzau3_10")
-comm.style.display = "none";
+function waitForElm(selector) {
+    return new Promise(resolve => {
+        if (document.querySelector(selector)) {
+            return resolve(document.querySelector(selector));
+        }
+
+        const observer = new MutationObserver(mutations => {
+            if (document.querySelector(selector)) {
+                observer.disconnect();
+                resolve(document.querySelector(selector));
+            }
+        });
+
+        // If you get "parameter 1 is not of type 'Node'" error, see https://stackoverflow.com/a/77855838/492336
+        observer.observe(document.body, {
+            childList: true,
+            subtree: true
+        });
+    });
+}
+waitForElm('._profilePreview_gzau3_63').then((profile) => {
+    profile.style.display = "none";
+});
+
+waitForElm('._link_gzau3_10').then((comm) => {
+    comm.style.display = "none";
+});
 </script>"""
 html(js)
 
